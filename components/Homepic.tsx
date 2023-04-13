9589fb2b664c540b7ad4349bccd647f7a7c61aa5
Kiran <kiranimran691@gmail.com>
2023-04-13 08:57:07 +0500

    given space home pic

--- components/Homepic.tsx
+++ components/Homepic.tsx
@@ -10,13 +10,13 @@ export const Homepic = () => {
     <section className="max-w-4xl mx-auto flex md:flex-row flex-col mt-16 text-justify"
      id="Home">
       {/* left start here  */}
-      <div className='p-5 max-w-lg leading-8'>
+      <div className='p-5 max-w-lg'>
         
 
       <h1 className='text-white text-4xl mb-8'>
         Hello, happy to see you!
       </h1>
-      <p className=' text-gray-300'>I help small and medium-sized businesses to reach out to the audience globally and grow their audience and brand recognition through web applications and social media.</p>
+      <p className=' text-gray-300 leading-8'>I help small and medium-sized businesses to reach out to the audience globally and grow their audience and brand recognition through web applications and social media.</p>
        {/* <button className='group flex item-center  bg-gradient-to-b from-cyan-300 text-black to-gray-600"" py-2 px-4 rounded-lg'>
         Profile
        <MdOutlineKeyboardArrowRight 
